9bd3f065b04c78abfcd168ee1fb773583ba5f982
Sardor Ergashaliyev <sardorergashaliyev@gmail.com>
2022-11-08 19:15:09 +0500

    Another Task

--- lib/main.dart
+++ lib/main.dart
@@ -17,13 +17,52 @@ class MyApp extends StatelessWidget {
             Align(
               alignment: Alignment.topCenter,
               child: Container(
-                height: 5000,
-                width: 5000,
-                color: Color.fromARGB(255, 53, 120, 255),
+                  height: 5000,
+                  width: 5000,
+                  color: Color.fromARGB(255, 0, 182, 88)),
+            ),
+            Positioned(
+              right: 150,
+              top: 140,
+              child: Align(
+                child: Container(
+                  height: 500,
+                  width: 200,
+                  color: Colors.yellow,
+                ),
               ),
             ),
             Positioned(
-              right: 200,
+                right: 170,
+                bottom: 270,
+                child: Align(
+                  child: Container(
+                    height: 300,
+                    width: 150,
+                    color: Colors.blue,
+                  ),
+                )),
+            Positioned(
+                right: 190,
+                bottom: 290,
+                child: Align(
+                  child: Container(
+                    child:  Text(
+                      'Child',
+                      style: TextStyle(
+                        color: Colors.black,
+                        decoration: TextDecoration.none,
+                        fontSize: 20,
+                        
+                      ),
+                    ),
+                    height: 250,
+                    width: 100,
+                    color: Color.fromARGB(255, 0, 182, 88),
+                  ),
+                )),
+            Positioned(
+              right: 0,
               child: Align(
                 alignment: Alignment.topCenter,
                 child: Container(
@@ -34,7 +73,7 @@ class MyApp extends StatelessWidget {
               ),
             ),
             Positioned(
-              bottom: 500,
+              bottom: 0,
               child: Container(
                 height: 100,
                 width: 5000,
