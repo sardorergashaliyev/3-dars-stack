ce8826884d4a8670bdc7c844e2385f45e3a1f7d3
Sardor Ergashaliyev <sardorergashaliyev@gmail.com>
2022-11-08 19:26:51 +0500

    Kubiklar

--- lib/main.dart
+++ lib/main.dart
@@ -14,72 +14,52 @@ class MyApp extends StatelessWidget {
         color: Color.fromARGB(255, 255, 255, 255),
         child: Center(
           child: Stack(children: [
-            Align(
-              alignment: Alignment.topCenter,
-              child: Container(
-                  height: 5000,
-                  width: 5000,
-                  color: Color.fromARGB(255, 0, 182, 88)),
-            ),
             Positioned(
               right: 150,
-              top: 140,
+              bottom: 260,
               child: Align(
                 child: Container(
-                  height: 500,
-                  width: 200,
-                  color: Colors.yellow,
-                ),
-              ),
-            ),
-            Positioned(
-                right: 170,
-                bottom: 270,
-                child: Align(
-                  child: Container(
-                    height: 300,
-                    width: 150,
-                    color: Colors.blue,
-                  ),
-                )),
-            Positioned(
-                right: 190,
-                bottom: 290,
-                child: Align(
-                  child: Container(
-                    child:  Text(
-                      'Child',
+                  child: Text('Green',
                       style: TextStyle(
-                        color: Colors.black,
-                        decoration: TextDecoration.none,
                         fontSize: 20,
-                        
-                      ),
-                    ),
-                    height: 250,
-                    width: 100,
-                    color: Color.fromARGB(255, 0, 182, 88),
-                  ),
-                )),
-            Positioned(
-              right: 0,
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: Container(
-                  height: 40000,
-                  width: 100,
-                  color: Colors.yellow,
+                          color: Colors.white,
+                          decoration: TextDecoration.none)),
+                  height: 200,
+                  width: 200,
+                  color: Colors.green,
                 ),
               ),
             ),
             Positioned(
-              bottom: 0,
-              child: Container(
-                height: 100,
-                width: 5000,
-                color: Colors.yellow,
-              ),
-            )
+                right: 110,
+                bottom: 220,
+                child: Align(
+                  child: Container(
+                    child: Text('Red',
+                      style: TextStyle(
+                        fontSize: 20,
+                          color: Colors.white,
+                          decoration: TextDecoration.none)),
+                    height: 200,
+                    width: 200,
+                    color: Colors.red,
+                  ),
+                )),
+            Positioned(
+                right: 60,
+                bottom: 180,
+                child: Align(
+                  child: Container(
+                    child: Text('Purple',
+                      style: TextStyle(
+                        fontSize: 20,
+                          color: Colors.white,
+                          decoration: TextDecoration.none)),
+                    height: 200,
+                    width: 200,
+                    color: Colors.purple,
+                  ),
+                )),
           ]),
         ),
       ),
